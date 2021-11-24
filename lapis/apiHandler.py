e175559d8f792f9c9dfe1907e8d93f5ac3f5f84d
Cappy Ishihara <cappy@cappuchino.xyz>
2021-11-24 09:06:36 +0700

    moved API code

--- lapis/apiHandler.py
+++ lapis/apiHandler.py
@@ -11,6 +11,7 @@ import lapis.api.builds
 import lapis.api.users
 import lapis.api.session
 import lapis.api.workers
+import lapis.api.tasks
 #The funny blueprints, idk i never used flask before
 
 from flask import Blueprint
@@ -35,6 +36,7 @@ app.config["DEBUG"] = True
 # set flask port to config
 
 app.register_blueprint(lapis.api.builds.builds, url_prefix=baseurl+'/builds')
+app.register_blueprint(lapis.api.tasks.tasks, url_prefix=baseurl+'/tasks')
 #app.register_blueprint(lapis.api.users.users, url_prefix=baseurl+'/users')
 #app.register_blueprint(lapis.api.session.session, url_prefix=baseurl+'/session')
 #app.register_blueprint(lapis.api.workers.workers, url_prefix=baseurl+'/workers')
